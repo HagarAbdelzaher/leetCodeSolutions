69b634e39ab0f6d9f6106e169634b40625522dd1
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2023-06-03 16:29:52 +0300

    Time: 0 ms (100.00%), Space: 6.4 MB (50.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,27 +3,30 @@ public:
     bool isValid(string s) {
         if(s.size()%2!=0)
             return false;
-        
-        
         stack<char> st;
+        unordered_map<char,char> parents = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'},
+        };
         for(int i=0 ; i<s.size() ; i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+            
+            if(parents.find(s[i])!= parents.end()) // if the current character is closing 
+            {
+                if(st.empty())  // i am pushing a closing parenthesis and stack is empty
+                {
+                    return false;
+                }
+                if(st.top()!= parents[s[i]]) // if current char's parent not on the top of the stack  
+                {
+                      return false;
+                }
+                
+                st.pop();
+            }
+            else
             {
                 st.push(s[i]);
-            }
-            else {
-                if(st.empty())
-                {
-                    return false;
-                }
-                else if((s[i]==')' && st.top()!= '(')|| (s[i]=='}' && st.top()!= '{') || (s[i]==']' && st.top()!= '['))
-                {
-                    return false;
-                }
-                else{
-                    
-                    st.pop();
-                }
             }
         }
         return st.empty();
